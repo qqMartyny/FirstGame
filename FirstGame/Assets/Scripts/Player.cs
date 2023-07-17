d166bc267df4043b5069ac3f96822f2b4191ed5b
qqMartyny <ilyanin3004@mail.ru>
2023-07-17 09:45:12 +0300

    Third commit
    -Add animations
    -Add some scripts

---
+++ FirstGame/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 step = new Vector3(hor, ver, 0);
+
+        animator.SetFloat("Horizontal", hor);
+        animator.SetFloat("Vertical", ver);
+        animator.SetFloat("Speed", (hor * hor + ver * ver));
+
+        transform.position += step * moveSpeed * Time.deltaTime;
+
+
+
+    }
+}
---
+++ FirstGame/Assets/Scripts/Zombie.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private float moveSpeed;
+    private Transform target;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        target = FindObjectOfType<Player>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        transform.position += direction * moveSpeed * Time.deltaTime;
+
+        animator.SetFloat("Horizontal", direction.x);
+        animator.SetFloat("Vertical", direction.y);
+        animator.SetFloat("Speed", direction.sqrMagnitude);
+    }
+}
